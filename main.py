51f3141697df7756f062a91fd7d0b005e3b74352
sun980207 <112291329+sun980207@users.noreply.github.com>
2022-08-29 16:57:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -51,5 +51,6 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 wea, temperature, wind = get_weather()
 data = {"diqu":{"value":city, "color":get_random_color()},"riqi":{"value":today, "color":get_random_color()},"xingqi":{"value":week, "color":get_random_color()},"weather":{"value":wea, "color":get_random_color()},"temperature":{"value":temperature, "color":get_random_color()},"wind":{"value":wind, "color":get_random_color()},"love_days":{"value":get_count(), "color":get_random_color()},"birthday_left":{"value":get_birthday(), "color":get_random_color()},"words":{"value":get_words(), "color":get_random_color()}}
+data = json.dumps(data, default=str, ensure_ascii=False)
 res = wm.send_template(user_id, template_id, data)
 print(res)
